254839d4c9d9169ac279ea5f0bf36d498abcc16e
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2022-02-28 21:15:21 -0700

    added learn more animation

--- projects/national-park/js/index.js
+++ projects/national-park/js/index.js
@@ -1,1 +1,36 @@
+// show and hide menu
 document.querySelector('#menuButton').addEventListener("click", () => document.querySelector('nav ul').classList.toggle("hidden"))
+
+// expand cards
+document.querySelectorAll('.learn-more').forEach((button) => {
+    button.addEventListener("click", ({currentTarget}) => {
+        let cardToExpand
+        switch (currentTarget.id) {
+            case "learn-more-bighorn":
+                cardToExpand = "bighorn"
+                break
+            case "learn-more-condor":
+                cardToExpand = "condor"
+                break
+            case "learn-more-tarantula":
+                cardToExpand = "tarantula"
+                break
+        }
+        document.querySelector('.cards').style.display = "block"
+        document.querySelector(`.${cardToExpand}`).classList.add("expanded")
+        document.querySelectorAll('.card').forEach(card => {
+            if (!card.classList.contains(cardToExpand)) card.classList.add("hidden")
+        })
+    })
+})
+
+document.querySelectorAll(".hide").forEach(button => {
+    button.addEventListener("click", () => {
+        document.querySelector('.cards').style.display = "grid"
+        document.querySelectorAll('.card').forEach(card => {
+            card.classList.remove("hidden")
+            card.classList.remove("expanded")
+        })
+        document.querySelectorAll('.more-info').forEach(section => section.classList.add("hidden"))
+    })
+})
